3db121d4305227b2310c5bf0ececdb9732045c49
AYBA46 <ayoubbadri39@gmail.com>
2025-09-03 10:43:04 +0200

    first Challenge1_Boucles

--- Challenge1_Boucles.c
+++ Challenge1_Boucles.c
@@ -9,8 +9,8 @@ int main() {
     scanf("%d",&num);
 
     for(i = 1 ; i <=10 ; i++){
-        printf("%d * %d = %d/n", num, i, num*i);
-        return 0;
+        printf("%d * %d = %d\n", num, i, num*i);
     }
+    return 0;
 
 }
